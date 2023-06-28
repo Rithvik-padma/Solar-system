8086f8ded9b8f617ec2be8acfc01fb6d6ad789a8
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-06-29 03:57:02 +0530

    Basic box animation

---
+++ learning-js-three/src/main.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three';
+
+const renderer = new THREE.WebGLRenderer();
+console.log("Threejs being used here")
+
+renderer.setSize(window.innerWidth, window.innerHeight-0.1);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth/(window.innerHeight-0.1),
+    0.1,
+    1000
+)
+
+// const axesHelper = new THREE.AxesHelper(2);
+// scene.add(axesHelper);
+
+camera.position.set(0, 0.5, 7)
+
+const boxGeometry = new THREE.BoxGeometry()
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+const animate = () => {
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+    scene.rotation.y += 0.01;
+    scene.rotation.x += 0.01;
+    if(box.position.z <= 2.5) box.position.z += 0.01;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate)
