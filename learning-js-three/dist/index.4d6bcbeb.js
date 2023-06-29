215d90235aa32d341084b6438ad42796188312bb
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-06-29 23:18:29 +0530

    Add shadow effects and spotlight controls

--- learning-js-three/dist/index.4d6bcbeb.js
+++ learning-js-three/dist/index.4d6bcbeb.js
@@ -578,7 +578,7 @@ var _three = require("three");
 var _orbitControlsJs = require("three/examples/jsm/controls/OrbitControls.js");
 var _datGui = require("dat.gui");
 const renderer = new _three.WebGLRenderer();
-console.log("Threejs being used here");
+renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight - 0.1);
 document.body.appendChild(renderer.domElement);
 const scene = new _three.Scene();
@@ -594,38 +594,66 @@ const boxMaterial = new _three.MeshBasicMaterial({
 });
 const box = new _three.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
-box.position.x = 9;
 const planeGeometry = new _three.PlaneGeometry(30, 30);
-const planeMaterial = new _three.MeshBasicMaterial({
+const planeMaterial = new _three.MeshStandardMaterial({
     color: 0xFFFFFF,
     side: _three.DoubleSide
 });
 const plane = new _three.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = Math.PI * -0.5;
+plane.receiveShadow = true;
 const gridHelper = new _three.GridHelper(30, 30);
 scene.add(gridHelper);
 const sphereGeometry = new _three.SphereGeometry(4, 100, 100);
-const sphereMaterial = new _three.MeshBasicMaterial({
+const sphereMaterial = new _three.MeshStandardMaterial({
     color: 0x0FF000
 });
 const sphere = new _three.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
+sphere.position.set(-10, 0, 5);
+sphere.castShadow = true;
+const ambientLight = new _three.AmbientLight(0x333333);
+scene.add(ambientLight);
+// const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.2)
+// scene.add(directionalLight)
+// directionalLight.position.set(-40, 50, 20)
+// directionalLight.castShadow = true;
+// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 3);
+// scene.add(directionalLightHelper)
+// const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(dLightShadowHelper);
+const spotLight = new _three.SpotLight(0xFFFFFF);
+scene.add(spotLight);
+spotLight.position.set(-50, 100, 0);
+spotLight.castShadow = true;
+const sLightHelper = new _three.SpotLightHelper(spotLight);
+scene.add(sLightHelper);
 const gui = new _datGui.GUI();
 const options = {
     sphereColor: "#ffea00",
     wireframe: false,
-    speed: 0.01
+    speed: 0.01,
+    angle: 0.2,
+    penumbra: 0,
+    intensity: 1
 };
 gui.addColor(options, "sphereColor").onChange((e)=>sphere.material.color.set(e));
 gui.add(options, "wireframe").onChange((e)=>sphere.material.wireframe = e);
 gui.add(options, "speed", 0.01, 0.1);
+gui.add(options, "angle", 0, 1);
+gui.add(options, "penumbra", 0, 1);
+gui.add(options, "intensity", 0, 1);
 let step = 0;
 const animate = (time)=>{
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
     step += options.speed;
     sphere.position.y = 3.8 + 8 * Math.abs(Math.sin(step));
+    spotLight.angle = options.angle;
+    spotLight.penumbra = options.penumbra;
+    spotLight.intensity = options.intensity;
+    sLightHelper.update();
     renderer.render(scene, camera);
 };
 renderer.setAnimationLoop(animate);
--- learning-js-three/src/main.js
+++ learning-js-three/src/main.js
@@ -3,7 +3,8 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui';
 
 const renderer = new THREE.WebGLRenderer();
-console.log("Threejs being used here")
+
+renderer.shadowMap.enabled = true;
 
 renderer.setSize(window.innerWidth, window.innerHeight-0.1);
 
@@ -30,10 +31,9 @@ const boxGeometry = new THREE.BoxGeometry(2, 2, 2)
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FFF0});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
-box.position.x = 9;
 
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({
+const planeMaterial = new THREE.MeshStandardMaterial({
     color: 0xFFFFFF,
     side: THREE.DoubleSide
 })
@@ -41,29 +41,60 @@ const planeMaterial = new THREE.MeshBasicMaterial({
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = Math.PI * -0.5;
+plane.receiveShadow = true;
 
 const gridHelper = new THREE.GridHelper(30, 30);
 scene.add(gridHelper)
 
 const sphereGeometry = new THREE.SphereGeometry(4, 100 , 100);
-const sphereMaterial = new THREE.MeshBasicMaterial({
+const sphereMaterial = new THREE.MeshStandardMaterial({
     color: 0x0FF000
 })
+
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
+sphere.position.set(-10, 0, 5)
+sphere.castShadow = true;
+
+const ambientLight = new THREE.AmbientLight(0x333333)
+scene.add(ambientLight)
+
+// const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.2)
+// scene.add(directionalLight)
+// directionalLight.position.set(-40, 50, 20)
+// directionalLight.castShadow = true;
+
+// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 3);
+// scene.add(directionalLightHelper)
+
+// const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(dLightShadowHelper);
+
+const spotLight = new THREE.SpotLight(0xFFFFFF);
+scene.add(spotLight);
+spotLight.position.set(-50, 100, 0);
+spotLight.castShadow = true;
+
+const sLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(sLightHelper);
 
 const gui = new dat.GUI();
 
 const options = {
     sphereColor: '#ffea00',
     wireframe: false,
-    speed: 0.01
+    speed: 0.01,
+    angle: 0.2,
+    penumbra: 0,
+    intensity: 1
 }
 
 gui.addColor(options, 'sphereColor').onChange((e) => sphere.material.color.set(e))
 gui.add(options, 'wireframe').onChange((e) => sphere.material.wireframe = e)
 gui.add(options, 'speed', 0.01, 0.1)
-
+gui.add(options, 'angle', 0 , 1)
+gui.add(options, 'penumbra', 0, 1)
+gui.add(options, 'intensity', 0, 1)
 let step = 0;
 
 const animate = (time) => {
@@ -71,6 +102,12 @@ const animate = (time) => {
     box.rotation.y = time/1000;
     step += options.speed
     sphere.position.y = 3.8 + 8 * Math.abs(Math.sin(step))
+
+    spotLight.angle = options.angle;
+    spotLight.penumbra = options.penumbra;
+    spotLight.intensity = options.intensity;
+    sLightHelper.update()
+
     renderer.render(scene, camera);
 }
 
