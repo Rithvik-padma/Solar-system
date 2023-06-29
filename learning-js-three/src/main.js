73238a338f14e856f006ee2e1edd8e78c2765609
Rithvik-padma <99183441+Rithvik-padma@users.noreply.github.com>
2023-06-29 18:30:58 +0530

    Add the plane to the scene

--- learning-js-three/src/main.js
+++ learning-js-three/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 const renderer = new THREE.WebGLRenderer();
 console.log("Threejs being used here")
@@ -10,28 +11,33 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(
-    75, 
+    45, 
     window.innerWidth/(window.innerHeight-0.1),
     0.1,
     1000
 )
 
-// const axesHelper = new THREE.AxesHelper(2);
-// scene.add(axesHelper);
+const orbit = new OrbitControls(camera, renderer.domElement);
 
-camera.position.set(0, 0.5, 7)
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 
-const boxGeometry = new THREE.BoxGeometry()
+camera.position.set(-10, 30, 30)
+orbit.update()
+
+const boxGeometry = new THREE.BoxGeometry(4, 4, 4)
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FFF0});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+
 const animate = () => {
     box.rotation.x += 0.01;
     box.rotation.y += 0.01;
-    scene.rotation.y += 0.01;
-    scene.rotation.x += 0.01;
-    if(box.position.z <= 2.5) box.position.z += 0.01;
     renderer.render(scene, camera);
 }
 
